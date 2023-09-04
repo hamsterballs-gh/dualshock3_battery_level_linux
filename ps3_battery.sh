48ebe1a1099201ee5b74290be9ebd4125ebfcf80
hamsterballs-gh <103415261+hamsterballs-gh@users.noreply.github.com>
2023-09-04 13:19:53 +0100

    Added controller count text

--- ps3_battery.sh
+++ ps3_battery.sh
@@ -6,7 +6,6 @@
 # A simple bash script to print the battery levels of attached and supported controllers
 
 # A standard user should be able to run this as normal, root is not required
-# Make it executable using chmod +x, and then run with ./ps3_battery.sh
 
 # This utilises the default Linux behaviour of allowing access to controller information
 # in /sys/class/power_supply
@@ -16,14 +15,20 @@
 # For each controller, get the capacity (read the capacity file)
 # Use echo to display it nicely
 
+controllers_seen=0
 
 for d in /sys/class/power_supply/* ; do
 	if [ -d "$d" ]; then
-
+		controllers_seen=$((controllers_seen+1))
 		bat_name=$(basename $d)
 		bat_capacity=$(cat $d/capacity)
 		echo "$bat_name -- $bat_capacity%"
 	fi
 done
 
+if [ $controllers_seen -eq 1 ]; then
+	echo "$controllers_seen controller was detected"
+else
+	echo "$controllers_seen controllers were detected"
+fi
 
